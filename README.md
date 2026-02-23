6efd616f7ac4195499c53813cb501d0592bfbae1
tech4life <tech4life@pop-os.lan>
2026-02-23 08:06:57 -0700

    functions: display first and last name

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+This repository is part of the Rust programming language course.
+
+## Activities
+The activities can be ran by executing `cargo run --bin a1` in your terminal, where `a1` is the name of the activity file.
+
---
+++ src/bin/a1.rs
@@ -0,0 +1,22 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn first_name() {
+    println!("First Name");
+}
+
+fn last_name() {
+    println!("Last Name");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
