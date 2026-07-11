35e6cb819bc2ddbecd9d4c17e2ef59e5e7d44f25
fayezosaadi <26156279+fayezosaadi@users.noreply.github.com>
2026-07-11 12:01:10 -0600

    option

---
+++ src/bin/a16.rs
@@ -0,0 +1,29 @@
+// Topic: Option
+//
+// Requirements:
+// * Print out the details of a mary's locker assignment
+// * Lockers use numbers and are optional for students
+//
+// Notes:
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+
+fn main() {
+    let mary = Student {
+        name: "Mary".to_owned(),
+        locker: Some(43),
+    };
+
+    println!("student: {:?}", mary.name);
+    match mary.locker {
+        Some(num) => println!("locker number: {:?}", num),
+        None => println!("no locker assigned"),
+    };
+}
